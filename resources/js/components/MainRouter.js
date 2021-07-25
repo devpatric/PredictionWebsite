fb237c9e7518e971eb2872639d685998755fd955
devpatric <patrickphillips578@gmail.com>
2021-07-25 16:21:27 +0300

    React Router configuration, added user, comment and post components

---
+++ resources/js/components/MainRouter.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import  {BrowserRouter as Router}  from 'react-router-dom';
+
+import Switch   from 'react-router-dom/Switch';
+//import  Router from 'react-router-dom/Router';
+import   Route from 'react-router-dom/Route';
+
+
+import Post from './Post/Post' ;
+import Comment from './Comment/Comment' ;
+import User from './User/User' ;
+
+
+
+class MainRouter extends React.Component{
+    constructor(props) {
+        super(props);
+
+ 
+    }
+
+    render() {
+        return (
+            <Router>
+                <div>
+
+
+                    {/* A <Switch> looks through its children <Route>s and
+                  renders the first one that matches the current URL. */}
+                    <Switch>
+                        <Route
+                            path="/post"
+                            render={(props) => (
+                                <Post  {...props}
+
+                                />)}
+
+                        >
+                        </Route>
+                        <Route
+                            path="/comment"
+                            render={(props) => (
+                                <Comment  {...props}
+
+                                />)}
+                        >
+
+                        </Route>
+                        <Route
+                            path="/user"
+                            render={(props) => (
+                                <User  {...props}
+
+                                />)}
+                        >
+
+                        </Route>
+
+
+
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
+
+        
+    
+}
+
+//ReactDOM.render(<Header />, document.getElementById('header'));
+ReactDOM.render(<MainRouter />, document.getElementById('example'));
+
+
+
---
+++ resources/js/components/Post/Post.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Posts extends React.Component{
+    constructor(props) {
+        super(props);
+
+ 
+    }
+    render(){
+        return(
+            <div> 
+                Posts
+            </div>
+        )
+    }
+}
